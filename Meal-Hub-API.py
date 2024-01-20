9cca21436fdbecb88b28f7dbf0ffcc7c71072ef7
hoangjam <j.hoag2080@gmail.com>
2024-01-20 01:50:01 -0500

    created .env file because some weird kid came and commented on our testing files

--- Meal-Hub-API.py
+++ Meal-Hub-API.py
@@ -1,31 +1,40 @@
 import spoonacular as sp
-import json
 from flask import Flask, request, jsonify
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+api_key = os.getenv('SPOONACULAR_API_KEY')
+api = sp.API(api_key)
 
-api = sp.API("35b3e7d707684b61bf0463a2834b2c86")
 app = Flask(__name__)
 
 # https://api.spoonacular.com/recipes/random?apiKey=35b3e7d707684b61bf0463a2834b2c86
 
-
 @app.route("/")
 def home():
-    return "Home"
+    return "Homepage / Dashboard"
+
+@app.route("/signup")
+def signUp():
+    return "sign up page spaceholder"
+
+@app.route("/login")
+def login():
+    return "login page spaceholder"
+
 
 @app.route("/search")
 def search():
     return "Search Bar"
-
 
 @app.route("/recipeInfo")
 def recipeInfo():
     # return "RecipeInfo"
     res = api.get_random_recipes()
     recipe = res.json()
-
     recipeURL = recipe["recipes"][0]["sourceUrl"]
-    #
+
     return recipeURL
 
 
