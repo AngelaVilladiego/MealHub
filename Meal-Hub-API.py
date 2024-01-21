059066877f427a17fd596a6b79514e5e244ae86a
AngelaVilladiego <villadia@sheridancollege.ca>
2024-01-21 08:55:05 -0500

    creating a meal plan works and returns correct json

--- Meal-Hub-API.py
+++ Meal-Hub-API.py
@@ -18,8 +18,6 @@ load_dotenv(find_dotenv())
 api_key = os.getenv('API_KEY')
 mongo_pwd = os.getenv('MONGO_PWD')
 
-print(mongo_pwd)
-
 api = sp.API(api_key)
 
 
@@ -45,7 +43,9 @@ def signUp():
     daysToPlan = user_data.get("daysToPlan", [])
 
     for day in daysToPlan:
-        mealPlan[day] = {}
+        mealPlan[day] = ""
+
+    print("signup mealplan: ", mealPlan)
 
     user = {
         "username": user_data["username"],
@@ -55,9 +55,14 @@ def signUp():
         "cuisine_preferences": user_data.get("cuisine_preferences", []),
         "mealPlan": mealPlan
     }
-
+    
     users_collection.insert_one(user)
-    return jsonify({"message": "User created", "user_id": str(user["_id"])}), 201
+    
+    user_id = str(user["_id"])
+    
+    helper_create_mealplan(user_id)
+    
+    return jsonify({"message": "User created", "user_id": user_id}), 201
 
 
 @app.route("/login", methods=['POST'])
@@ -91,55 +96,32 @@ def add_favourite():
     )
     return jsonify({"message": "Favourite added successfully!"}), 200
 
-@app.route("/user/<user_id>/create_mealplan", methods=['POST'])
-def create_mealplan(user_id):
+@app.route("/user/create_mealplan", methods=['POST'])
+def create_mealplan():
     data = request.json
     user_id = data["user_id"]
 
+    helper_create_mealplan(user_id)
     # Fetch the user from the database using the user_id
-    user = users_collection.find_one({"_id": user_id})
-    users_collection.insert_one(user)
+    res = helper_get_mealplan(user_id)
 
-    if not user:
-        return jsonify({"message": "User not found"}), 404
+    return jsonify(res)
 
-    mealPlan = user["mealPlan"]
+@app.route("/user/get_mealplan", methods=["GET"])
+def get_user_mealplan():
+    res = helper_get_mealplan(request.args.get("user_id"))
 
-    for day in mealPlan:
-        recipe_id = get_recipe_for_user(user_id)
-        recipe_dto = get_recipeDTO_from_id(recipe_id)
-        mealPlan[day] = recipe_dto
+    return res
 
-    return mealPlan
 
 
 # return one recipeId based on the user's dietary restrictions and cuisine preferences
 @app.route("/user/<user_id>/get_recipe_for_user", methods=["GET"])
 def get_recipe_for_user(user_id):
     # user = users_collection.find_one({"_id": user_id})
-    user = users_collection.find_one({"_id": ObjectId(user_id)})
+    res = helper_get_recipe_for_user(user_id)
 
-    if not user:
-        return jsonify({"message": "User not found"})
-
-    dietary_restriction = user.get("dietary_restriction", [])
-    cuisine_preferences = user.get("cuisine_preferences", [])
-
-    # https://api.spoonacular.com/recipes/random?number=1&include-tags=vegetarian,dessert&exclude-tags=quinoa&apiKey={api_key}
-
-
-    response = requests.get(
-        # Uncommet this when it's needed
-        # f"https://api.spoonacular.com/recipes/random?number=1&include-tags={cuisine_preferences}&exclude-tags={dietary_restriction}&apiKey={api_key}")
-
-        # 'https://api.spoonacular.com/recipes/random?number=1&include-tags=beef&apiKey=35b3e7d707684b61bf0463a2834b2c86'
-        f'https://api.spoonacular.com/recipes/random?number=1&include-tags=beef&apiKey={api_key}'
-    )
-    data = json.loads(response.text)
-    recipe_id = data["recipes"][0]["id"]
-    print(recipe_id)
-    converted = str(recipe_id)
-    return jsonify({"recipe_id": converted})
+    return res
 
 # Retrieve a recipe by ID
 @app.route('/get_recipebyid', methods=['GET'])
@@ -156,9 +138,7 @@ def get_recipeById():
 
 
 # create a recipe dto object from the recipe id
-@app.route('/get_recipedto', methods=['GET'])
-def get_recipeDTO_from_id():
-    recipe_id = request.args.get("recipe_id")
+def helper_get_recipeDTO_from_id(recipe_id):
     url = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}"
     response = requests.get(url)
 
@@ -174,9 +154,11 @@ def get_recipeDTO_from_id():
             ingredients=[ingredient["name"] for ingredient in data["extendedIngredients"]]
         )
 
-        return jsonify(recipe_dto.to_dict())
+        print("++++ ", recipe_dto.to_dict())
+
+        return recipe_dto.to_dict()
     else:
-        return jsonify({"error": "Couldn't find recipe"})
+        return {"error": "Couldn't find recipe"}
 
 
 
@@ -195,5 +177,82 @@ def get_favourites(user_id):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+def helper_create_mealplan(user_id):
+    print("creating mealplan....")
+    user = users_collection.find_one({"_id": ObjectId(user_id)})
+
+    print("user: ", user['mealPlan'])
+    
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+    
+    mealPlan = user['mealPlan']
+    print("got mealplan: ", mealPlan)
+
+    for day in mealPlan:
+        res = helper_get_recipe_for_user(user_id)
+        print("+????", res)
+        mealPlan[day] = res
+
+    print("created mealplan: ", mealPlan)
+
+    # users_collection.find_one_and_update({"_id": user_id}, {"$set": {"mealPlan": mealPlan}})
+    users_collection.update_one(
+        {"_id": ObjectId(user_id)},
+        {"$set": {"mealPlan": mealPlan}}
+    )
+
+def helper_get_mealplan(user_id):
+    user = users_collection.find_one({"_id": ObjectId(user_id)})
+    # users_collection.insert_one(user)
+
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    mealPlan = user["mealPlan"]
+    print("HERE: ", mealPlan)
+
+    mealPlanDTO = {}
+
+    for day in mealPlan:
+        print("day: ", day)
+        recipe_id = mealPlan[day]
+        print("recipe_id: ", recipe_id)
+        recipe_dto = helper_get_recipeDTO_from_id(recipe_id)
+        print("recipe_dto:" , recipe_dto)
+        mealPlanDTO[day] = recipe_dto
+
+    print("+++check me :D ****", mealPlanDTO)
+    return mealPlanDTO
+
+def helper_get_recipe_for_user(user_id):
+    user = users_collection.find_one({"_id": ObjectId(user_id)})
+
+    if not user:
+        return jsonify({"message": "User not found"}, 404)
+
+    dietary_restriction = user.get("dietary_restriction", [])
+    cuisine_preferences = user.get("cuisine_preferences", [])
+
+    # https://api.spoonacular.com/recipes/random?number=1&include-tags=vegetarian,dessert&exclude-tags=quinoa&apiKey={api_key}
+
+
+    response = requests.get(
+        # Uncommet this when it's needed
+        # f"https://api.spoonacular.com/recipes/random?number=1&include-tags={cuisine_preferences}&exclude-tags={dietary_restriction}&apiKey={api_key}")
+
+        # 'https://api.spoonacular.com/recipes/random?number=1&include-tags=beef&apiKey=35b3e7d707684b61bf0463a2834b2c86'
+        f'https://api.spoonacular.com/recipes/random?number=1&include-tags=beef&apiKey={api_key}'
+    )
+    data = json.loads(response.text)
+    recipe_id = data["recipes"][0]["id"]
+    print("got recipe id helper get recipe for user: ", recipe_id)
+    converted = str(recipe_id)
+    print("conv: ", converted)
+    # return jsonify({"recipe_id": converted})
+    return converted
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
